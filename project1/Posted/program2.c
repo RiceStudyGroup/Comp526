ce1bd815e093de8819367adf538de3464d650336
JobQiu <congminqi@gmail.com>
2019-01-26 07:07:41 -0600

    ...

--- project1/Posted/program2.c
+++ project1/Posted/program2.c
@@ -23,16 +23,16 @@ loadProgram() {
 
 
     insMEM[0] = 0x14840001; //  ADDI R4, R4, 1   top1: index2++ 
-    insMEM[5] = 0x10810000; //  LOAD R1, 0(R4)         element2 = MEM[index2]
-    insMEM[10] = 0x14420001; // ADDI R2, R2, 1         index1++
-    insMEM[15] = 0x10400000; // LOAD R0, 0(R2)         element1 = MEM[index1]
-    insMEM[20] = 0x04012800; // ADD R5, R0, R1         temp = element1 + element2
-    insMEM[25] = 0x18C50000; // STORE R5, 0(R6)        MEM[index3] = temp
-    insMEM[30] = 0x14C60001; // ADDI R6, R6, 1         index3++
-    insMEM[35] = 0x1508FFFF; // ADDI R8, R8, -1        count--
-    insMEM[36] = 0x2500FFDC; // BNEZ R8 -36            if (count != 0) goto top
-    insMEM[40] = 0x00000000; // NOP 
-    insMEM[45] = 0x28000000; //HALT
+    insMEM[1] = 0x10810000; //  LOAD R1, 0(R4)         element2 = MEM[index2]
+    insMEM[2] = 0x14420001; // ADDI R2, R2, 1         index1++
+    insMEM[3] = 0x10400000; // LOAD R0, 0(R2)         element1 = MEM[index1]
+    insMEM[4] = 0x04012800; // ADD R5, R0, R1         temp = element1 + element2
+    insMEM[5] = 0x18C50000; // STORE R5, 0(R6)        MEM[index3] = temp
+    insMEM[6] = 0x14C60001; // ADDI R6, R6, 1         index3++
+    insMEM[7] = 0x1508FFFF; // ADDI R8, R8, -1        count--
+    insMEM[8] = 0x2500FFDC; // BNEZ R8 -36            if (count != 0) goto top
+    insMEM[9] = 0x00000000; // NOP 
+    insMEM[11] = 0x28000000; //HALT
 }
 
 
